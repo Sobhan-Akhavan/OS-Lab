3d68e26e5ef23f9b46900ee1b1ee67805251300b
sobhan <sobhan.akhavanm@gmail.com>
2021-11-06 00:09:46 +0330

    improve assagnment

--- Assignments/5/store.py
+++ Assignments/5/store.py
@@ -1,4 +1,4 @@
-from os import spawnl
+from os import remove, spawnl
 
 PRODUCTS = []
 
@@ -16,12 +16,13 @@ def load_data_from_file():
 
 def check_exist(pro):
     for product in PRODUCTS:
-        if str(pro) == product['code'] or pro == product['name']:
+        if pro[0] == product['code'] or pro[1] == product['name']:
             return product
     return False
 
+
 def add():
-    print('add new product whith below format')
+    print('Add/Edite new product whith below format')
     txt = input('code,name,price,count: ')
     new_product = txt.split(',')
     if check_exist(new_product) == False:
@@ -32,11 +33,24 @@ def add():
         print('duplicated production')
     load_data_from_file()
 
-def edit():
-    pass
+def edit(code):
+    if search(code) == False:
+        print("Product not found")
+        return
+    add()
+    delete(code)
 
-def delete():
-    pass
+
+def delete(code):
+    if search(code) == False:
+        print("Product not found")
+        return
+    with open("database.csv", "r") as f:
+        lines = f.read().split(',')
+    with open("database.csv", "w") as f:
+        for word in lines:
+            if word != code:
+                f.write(word + ',')
 
 def show_list():
     print('code\tname\tprice\tcount')
@@ -52,10 +66,11 @@ def search(var):
     return False
 
 def buy(code):
-    if check_exist(code) == False:
+    product = search(code)
+    if product == False:
         return False
 
-    product = search(code)
+    
     while True:
         x = int(input('Enter number of count or 0 to exit: '))
         if x == 0:
@@ -67,20 +82,23 @@ def buy(code):
             print('Your cost ', price)
             cost = int(input('Enter number of cost: '))
             if price == cost:
-                product['count'] = product['count'] - x
-                f = open('database.csv', 'a')
+                product['count'] = int(product['count']) - x
+                f = open('database.csv', 'r')
                 for row in f:
                     info = row[:-1].split(',')
                     if info[0] == code:
                         info[3] = product['count']
-                        f.write()
-                        f.close()
-                    
-                     
+                        break
+                with open("database.csv", "w") as f:
+
+                        for word in info:
+                            f.write(str(word) + ',')
+                        
     
 
 def save_and_exit():
-    pass
+    print('See you later')
+    quit()
 
 
 def show_menu():
@@ -105,15 +123,17 @@ while True:
         code = int(input('Enter code of production: '))
         edit(code)
     elif choice == 3:
-        delete()
+        code = int(input('Enter code of production: '))
+        delete(code)
     elif choice == 4:
         show_list()
     elif choice == 5:
-        var = input('Name of product: ')
+        var = input('Name or Code of product: ')
         if search(var) == False:
             print('Not found')
     elif choice == 6:
         code = input('Enter code of product: ')
         if buy(code) == False:
             print('Wrong product code or exit buying process')
-    
+    elif choice == 7:
+        save_and_exit()    
