588bec5ce6a96e78b10c7194749f9da64234890a
sobhan <sobhan.akhavanm@gmail.com>
2022-01-13 17:17:11 +0330

    add one italy person

---
+++ Assignments/15/1-inser.sql
@@ -0,0 +1,14 @@
+INSERT INTO Customers (name, city, age, country)
+VALUES ('Sobhan', 'Mashahd', '20' , 'Iran');
+
+INSERT INTO Customers (name, city, age, country)
+VALUES ('Sajjad', 'Mashhad', '30' , 'IRAN');
+
+INSERT INTO Customers (name, city, age, country)
+VALUES ('MIK', 'NY', '10' , 'ITALY');
+
+INSERT INTO Products (name, brand, price, count)
+VALUES ('pasta', 'zar' , '22000', '200');
+
+INSERT INTO Products (name, brand, price, count)
+VALUES ('pasta', 'tak', '44000', '300');
